170017de6ed5f4e03c863443a7294c3501602558
Victory-Hugo <2025320056@stu.cqmu.edu.cn>
2025-12-23 16:06:43 +0800

    版本号

--- seqmagick2/__init__.py
+++ seqmagick2/__init__.py
@@ -4,4 +4,4 @@ try:
     with open(path.join(path.dirname(__file__), 'data', 'ver')) as f:
         __version__ = f.read().strip().replace('-', '+', 1).replace('-', '.')
 except Exception as e:
-    __version__ = ''
+    __version__ = '1.0.0'
